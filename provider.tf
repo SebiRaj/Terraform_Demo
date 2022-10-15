78141a8681e3db44cfcfce87ac506e59291e1c47
Sebi <s.sebastin@gmail.com>
2022-10-15 16:35:40 +0530

    added Terraform basicfiles

---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+region      = "us-west-2"
+access_key  = "AKIAYPYDKGIWBFBO4PXO"
+secret_key  = "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"
+}
---
+++ s3bkt.tf
@@ -0,0 +1,8 @@
+resource "aws_s3_bucket" "bkt100" {
+  bucket = "mybkt100tf"
+
+  tags = {
+    Name        = "tf100bkt"
+    Environment = "DevEnv"
+  }
+}
